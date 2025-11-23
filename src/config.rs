f79ea49c7a77546261908827d5042f0112c24c1f
jtong1231 <jtong5995@gmail.com>
2025-11-23 20:25:17 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cdn.32njrfnbjksdv.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["tx.32njrfnbjksdv.top"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
 pub const RENDEZVOUS_PORT: i32 = 43506;
 pub const RELAY_PORT: i32 = 43507;
