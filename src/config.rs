f9f303ed77124dc102c2f959e75e59ae8ebeef5b
jtong1231 <jtong5995@gmail.com>
2025-11-25 18:00:59 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cnd.wojddw2dd.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cd.wojddw2dd.top"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
 pub const RENDEZVOUS_PORT: i32 = 43706;
 pub const RELAY_PORT: i32 = 43707;
